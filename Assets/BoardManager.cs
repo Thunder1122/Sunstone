a174f5e66f553fea74fbe952b45138c763e06044
Tushar Chandra <TusharChandra2018@u.northwestern.edu>
2017-02-19 21:01:08 -0600

    Update documentation

--- Assets/BoardManager.cs
+++ Assets/BoardManager.cs
@@ -7,9 +7,11 @@ public class BoardManager : MonoBehaviour {
     public static readonly int ROWS = 10;
     public static readonly int COLUMNS = 10;
     public GameObject[,] BoardArray = new GameObject[ROWS, COLUMNS];
+    
+    // Track players
     public Player DemoPlayer;
-    public int CurrCellManaType;
 
+    // Index of empty sprite
     public int EmptyIndex = 6;
 
     // Default, when nothing selected, is -1
@@ -45,74 +47,88 @@ public class BoardManager : MonoBehaviour {
     }
 
     /// <summary>
-    /// Generates a weighted random index, used for board generation
-    /// then assigns the sprite associated to that index to the passed object
+    /// Generate a random sprite for specified cell
     /// </summary>
     internal void RandomSprite(GameObject cell) {
+        // Using grabbag randomness -- each val corresponds to a sprite type, and
+        // those that have more keys associated with them (0 - 4) are more likely
+        // to appear.
+        //
+        // Mana types 0 through 4 have 20% chance; types 5 and 6 (shield / health) 10%
         Dictionary<int, int> GrabBag = new Dictionary<int, int> {
             {0,0}, {1,0}, {2,1}, {3,1}, {4,2}, {5,2}, {6,3}, {7,3}, {8,4}, {9,5}
             };
+        
+        // Choose and apply random sprite
         int RandIndex = GrabBag[Random.Range(0, 10)];
         cell.GetComponent<BoardCell>().SpriteIndex = RandIndex;
         cell.GetComponent<SpriteRenderer>().sprite = sprites[RandIndex];
+
         return;
     }
 
     /// <summary>
-    /// Iterates over the board and finds all matches of 3 or more
+    /// Find all matches of 3 or more on the board
     /// </summary>
-    /// <returns>A list containing all gameObjects involved in matches</returns>
+    /// <returns>List containing all GameObjects involved in matches</returns>
     internal List<GameObject> FindMatches() {
-        //Check horizontal matches
+        // Strategy: iterate over rows checking horizontal matches, then columns
+        // checking vertical matches. In each, look for chains of cells that have
+        // the same sprite. If the chain is broken, and it had length >= 3, add
+        // the preceding cells to the match.
+
+        // Iterate over rows, checking horizontal matches
         for (int row = 0; row < ROWS; row++) {
-            //Set initial values
+            // Set initial chain values
             int ChainLength = 1;
             int CurrentSprite = -1;
 
-            //Go through each cell in a row and compare it to cells left and right
+            // Compare cells in row to left and right neighbors
             for (int col = 0; col < COLUMNS; col++) {
-                //If its the same sprite and non-empty, update chain length and move on
+                // If same sprite and non-empty, update chain length
                 if ((CurrentSprite == BoardArray[row, col].GetComponent<BoardCell>().SpriteIndex) && 
                     (BoardArray[row,col].GetComponent<BoardCell>().SpriteIndex != EmptyIndex)) {
                     ChainLength += 1;
                 }
 
-                //Otherwise, its a new sprite and breaks the chain, need to check if
-                //ChainLength is enough to be a "match" and then reset value appropriately
+                // Otherwise, it's a new sprite, and breaks the chain.
                 else {
+                    // Check if ChainLength is >= 3, to be a match, then reset.
                     if (ChainLength > 2) {
                         for (int ii = 1; ii <= ChainLength; ii++) {
                             BoardArray[row, col - ii].GetComponent<BoardCell>().Matched = true;
                         }
                     }
+
                     ChainLength = 1;
                     CurrentSprite = BoardArray[row, col].GetComponent<BoardCell>().SpriteIndex;
                 }
             }
-            //If there was a match at the very end, we need to catch it
+
+            // Catch possible match at the end of the row
             if (ChainLength > 2) {
                 for (int ii = 1; ii <= ChainLength; ii++) {
                     BoardArray[row, COLUMNS - ii].GetComponent<BoardCell>().Matched = true;
                 }
             }
         }
-        //Check vertical matches
+        // Iterate over columns, checking vertical matches
         for (int col = 0; col < COLUMNS; col++) {
-            //Set initial values
+            // Set initial chain values
             int ChainLength = 1;
             int CurrentSprite = -1;
 
-            //Go through each cell in a column and compare it to cells above and below
+            // Compare cells in column to top and bottom neighbors
             for (int row = 0; row < COLUMNS; row++) {
-                //If its the same sprite, update chain length and move on
+                // If same sprite and non-empty, update chain length
                 if ((CurrentSprite == BoardArray[row, col].GetComponent<BoardCell>().SpriteIndex) &&
                     (BoardArray[row, col].GetComponent<BoardCell>().SpriteIndex != EmptyIndex)) {
                     ChainLength += 1;
                 }
 
-                //Otherwise, its a new sprite and breaks the chain, need to check if
-                //ChainLength is enough to be a "match" and then reset value appropriately
+                // Otherwise, it's a new sprite, and breaks the chain.
                 else {
+                    // Check if ChainLength is >= 3, to be a match, then reset.
                     if (ChainLength > 2) {
                         for (int ii = 1; ii <= ChainLength; ii++) {
                             BoardArray[row - ii, col].GetComponent<BoardCell>().Matched = true;
@@ -122,25 +138,29 @@ public class BoardManager : MonoBehaviour {
                     CurrentSprite = BoardArray[row, col].GetComponent<BoardCell>().SpriteIndex;
                 }
             }
-            //If there was a match at the very end, we need to catch it
+
+            // Catch possible match at the end of the row
             if (ChainLength > 2) {
                 for (int ii = 1; ii <= ChainLength; ii++) {
                     BoardArray[ROWS - ii, col].GetComponent<BoardCell>().Matched = true;
                 }
             }
         }
-        //Find all the cells marked part of matches and add them to our list
+
+        // Collect all cells that were matched
         List<GameObject> InMatch = new List<GameObject>();
+
         foreach (GameObject cell in BoardArray) {
             if (cell.GetComponent<BoardCell>().Matched) {
                 InMatch.Add(cell);
             }
         }
+
         return InMatch;
     }
 
     /// <summary>
-    /// Replaces the sprites of the cells passed with the empty sprite
+    /// Replace sprites of passed cells with empty sprite
     /// </summary>
     internal void RemoveMatches(List<GameObject> matches) {
         foreach (GameObject cell in matches) {
@@ -151,54 +171,65 @@ public class BoardManager : MonoBehaviour {
     }
 
     /// <summary>
-    /// Generates a biased-random starting board with no pre-existing matches
+    /// Generate starting board with no pre-existing matches
     /// </summary>
     internal void GenerateBoard() {
-        //Populate an initial board
+        // Populate an initial board
         foreach (GameObject cell in BoardArray) {
             RandomSprite(cell);
         }
 
-        //Validate the board by replacing any starting matches
+        // Replace any starting matches
         List<GameObject> Matches = FindMatches();
+
         while (Matches.Count != 0) {
             foreach (GameObject cell in Matches) {
                 RandomSprite(cell);
                 cell.GetComponent<BoardCell>().Matched = false;
             }
+
             Matches = FindMatches();
         }
+
         return;
     }
 
 
     internal void Start() {
-        // Instantiate a hand for demoing
+        // Instantiate a player for demo purposes
         DemoPlayer = GameObject.Find("Player1").transform.GetComponent<Player>();
 
         // Reset selected values
         SelectedRow = -1;
         SelectedColumn = -1;
 
-        // Stores each BoardCell into the board_array, separating board state from rendering
+        // Stores each BoardCell into the BoardArray, separating board state from rendering
         for (int row = 0; row < ROWS; row++) {
             for (int column = 0; column < COLUMNS; column++) {
                 BoardArray[row, column] = GameObject.Find(CoordToName(row, column));
             }
         }
 
-        // Initialize sprites to the board_array
+        // Initialize sprites to the BoardArray
         GenerateBoard();
     }
-
-    // Update is called once per frame
+    
+    /// <summary>
+    /// Find matches, award mana, remove matches
+    /// </summary>
     internal void Update () {
+        // Find matches
         List<GameObject> Matches = FindMatches();
+
+        // Give mana to player for matched cells
         int[] collected = new int[6];
         foreach (GameObject cell in Matches) {
             collected[cell.GetComponent<BoardCell>().SpriteIndex] += 1;
         }
+
         DemoPlayer.ChangeMana(collected);
+
+        // Remove matched cells from board
         RemoveMatches(Matches);
     }
 
